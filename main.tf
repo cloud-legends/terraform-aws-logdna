cee80d12d5491801ade889ca1fc008ac37086c26
Bart Vollebregt <3983932+bartvollebregt@users.noreply.github.com>
2019-09-24 12:30:12 +0200

    Add license and travis build file

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 /* Lambda execution role */
 resource "aws_iam_role" "lambda_execute_role" {
-  name = "${var.lambda_execute_role_name}"
+  name = var.lambda_execute_role_name
   assume_role_policy = "${file("${path.module}/policy/logdna_lambda_stream_role.json")}"
   force_detach_policies = true
 }
 
 data "template_file" "lambda_stream_policy" {
-  template = "${file("${path.module}/policy/logdna_lambda_stream_policy.json")}"
+  template = file("${path.module}/policy/logdna_lambda_stream_policy.json")
 }
 
 resource "aws_iam_role_policy" "lambda_container_policy" {
-  role        = "${aws_iam_role.lambda_execute_role.id}"
-  policy      = "${data.template_file.lambda_stream_policy.rendered}"
+  role        = aws_iam_role.lambda_execute_role.id
+  policy      = data.template_file.lambda_stream_policy.rendered
 }
 
 /* Lambda function */
@@ -20,12 +20,12 @@ resource "aws_lambda_function" "lambda_stream" {
   handler           = "logdna_cloudwatch.lambda_handler"
   runtime           = "python2.7"
   filename          = "${path.module}/lambda/stream_to_logdna.zip"
-  source_code_hash  = "${base64sha256(file("${path.module}/lambda/stream_to_logdna.zip"))}"
-  role              = "${aws_iam_role.lambda_execute_role.arn}"
+  source_code_hash  = base64sha256(file("${path.module}/lambda/stream_to_logdna.zip"))
+  role              = aws_iam_role.lambda_execute_role.arn
 
   environment {
     variables = {
-      LOGDNA_KEY = "${var.log_dna_key}"
+      LOGDNA_KEY = var.log_dna_key
     }
   }
 
@@ -36,17 +36,17 @@ resource "aws_lambda_function" "lambda_stream" {
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
   action         = "lambda:InvokeFunction"
-  function_name  = "${aws_lambda_function.lambda_stream.function_name}"
-  principal      = "${var.lambda_principal}"
-  source_arn     = "${var.log_group_arn}"
+  function_name  = aws_lambda_function.lambda_stream.function_name
+  principal      = var.lambda_principal
+  source_arn     = var.log_group_arn
 }
 
 /* Log subscription filter */
 resource "aws_cloudwatch_log_subscription_filter" "test_lambdafunction_logfilter" {
-  name            = "${var.log_subscription_filter}"
-  log_group_name  = "${var.log_group_name}"
-  filter_pattern  = "${var.filter_pattern}"
-  destination_arn = "${aws_lambda_function.lambda_stream.arn}"
+  name            = var.log_subscription_filter
+  log_group_name  = var.log_group_name
+  filter_pattern  = var.filter_pattern
+  destination_arn = aws_lambda_function.lambda_stream.arn
   distribution    = "ByLogStream"
 
   depends_on      = ["aws_lambda_permission.allow_cloudwatch"]
